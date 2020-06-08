3e1b980531b1b13a91b57449cdccd46f328f5861
TNabbefeldHBS <66482010+TNabbefeldHBS@users.noreply.github.com>
2020-06-08 16:51:02 -0500

    Update pre_migration_master.ps1
    Added simple error reporting and GUI capability for picking directory.

--- pre_migration_master.ps1
+++ pre_migration_master.ps1
@@ -1,7 +1,15 @@
-﻿
+﻿<#
 param (
 		        [string] $startDirectory = ""
       )
+#>
+
+# Display window to browse for folder
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+$FileBrowser.ShowDialog()
+$startDirectory = $FileBrowser.SelectedPath
+
 
 Install-Module PSSQLite
 Import-Module PSSQLite
@@ -14,37 +22,37 @@ $global:DataSource = '.\FileToOneDrive.db'
 
 function GetUsers($batchNumber)
 {
-  $users = $null
-  write-host $batchNumber
-  $query = 'SELECT Files_Batch_Users.ADhomeDirectory,Files_Batch_Users.Id FROM Files_Batch_Users WHERE Files_Batch_Users.BatchNumber = ' + $batchNumber + ''
-  write-host "Query:" $query -ForegroundColor Green
-  $users = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
-  write-host "running query"
-  #write-host $users
-  return $users
+	$users = $null
+	write-host $batchNumber
+	$query = 'SELECT Files_Batch_Users.ADhomeDirectory,Files_Batch_Users.Id FROM Files_Batch_Users WHERE Files_Batch_Users.BatchNumber = ' + $batchNumber + ''
+	write-host "Query:" $query -ForegroundColor Green
+	$users = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
+	write-host "running query"
+	#write-host $users
+	return $users
 }
 
 function GetOldOfficeDocuments($directory)
 {
-  $documents = $null
-  $query = "SELECT Files_OneDrive.Path as Path,Files_OneDrive.Id as Id,Files_OneDrive.OwnerId as OwnerId FROM Files_OneDrive, Files_Batch_Users WHERE (Extension = 'xls' OR Extension = 'doc' OR Extension = 'ppt') AND Files_Batch_Users.Id = Files_OneDrive.OwnerID AND Files_Batch_Users.ADhomeDirectory = '" + $directory + "'"
-  write-host "Query:" $query -ForegroundColor Green
-  $documents = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
-  write-host "running query"
-  #write-host $documents
-  return $documents
+	$documents = $null
+	$query = "SELECT Files_OneDrive.Path as Path,Files_OneDrive.Id as Id,Files_OneDrive.OwnerId as OwnerId FROM Files_OneDrive, Files_Batch_Users WHERE (Extension = 'xls' OR Extension = 'doc' OR Extension = 'ppt') AND Files_Batch_Users.Id = Files_OneDrive.OwnerID AND Files_Batch_Users.ADhomeDirectory = '" + $directory + "'"
+	write-host "Query:" $query -ForegroundColor Green
+	$documents = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
+	write-host "running query"
+	#write-host $documents
+	return $documents
 }
 
 function GetNewBatch($directory)
 {
-  $users = $null
-  write-host $batchNumber
-  $query = "SELECT Files_Batch_Users.ADhomeDirectory,Files_Batch_Users.Id FROM Files_Batch_Users WHERE Files_Batch_Users.ADhomeDirectory = '" + $directory + "'"
-  write-host "Query:" $query -ForegroundColor Green
-  $users = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
-  write-host "running query"
-  #write-host $users
-  return $users
+	$users = $null
+	write-host $batchNumber
+	$query = "SELECT Files_Batch_Users.ADhomeDirectory,Files_Batch_Users.Id FROM Files_Batch_Users WHERE Files_Batch_Users.ADhomeDirectory = '" + $directory + "'"
+	write-host "Query:" $query -ForegroundColor Green
+	$users = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
+	write-host "running query"
+	#write-host $users
+	return $users
 }
 
 function CreateNewDirectoryEntry($directory)
@@ -140,20 +148,34 @@ function OfficeConversionTest()
 
 function GeneratePostScanReport ($directory)
 {
- $users = $null
-  write-host $batchNumber
-  $query = "SELECT Id,SamAccountName,BatchNumber,ADHomeDirectory,OwnerId,FileCountDisk,FileCountCrawl,MacroCount,Extensions,FileSizeDisk,FileSizeCrawl,ErrorCount,OfficeErrorCount,OldOfficeCount,PathLengthCount,NoAccessCount,CreatedDate FROM Files_Batch_Users,Files_Users WHERE Files_Batch_Users.Id = Files_Users.OwnerId AND ADHomeDirectory = '" + $directory + "'"
-  write-host "Query:" $query -ForegroundColor Green
-  $report = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
-  write-host "running query"
-  #write-host $users
-  $timestamp =  get-date -f _MM_dd_HH_mm_ss
-  $logFile = ".\report_" + $timestamp + "$ownerId.csv"
-  $report  | Export-Csv $logFile
+	write-host $global:DataSource
+	$users = $null
+	write-host $batchNumber
+	$query = "SELECT Id,SamAccountName,BatchNumber,ADHomeDirectory,OwnerId,FileCountDisk,FileCountCrawl,MacroCount,Extensions,FileSizeDisk,FileSizeCrawl,ErrorCount,OfficeErrorCount,OldOfficeCount,PathLengthCount,NoAccessCount,CreatedDate FROM Files_Batch_Users,Files_Users WHERE Files_Batch_Users.Id = Files_Users.OwnerId AND ADHomeDirectory = '" + $directory + "'"
+	write-host "Query:" $query -ForegroundColor Green
+	$report = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
+	write-host "running query"
+	#write-host $users
+	$timestamp =  get-date -f _MM_dd_HH_mm_ss
+	$logFile = ".\report_" + $timestamp + "$ownerId.csv"
+	$report  | Export-Csv $logFile
+	
+
+	#Microsoft errors query
+	$query = "SELECT OwnerId,SamAccountName,BatchNumber,ADHomeDirectory,FileName,Extension,Path,Error
+			` FROM Files_Batch_Users, Files_OneDrive
+			` WHERE ADHomeDirectory = '$directory' AND Files_Batch_Users.id = Files_OneDrive.OwnerId AND Error <> '' AND Error <> ' '"
+	write-host "Query:" $query -ForegroundColor Green
+	$report = Invoke-SqliteQuery -Query $query -DataSource $global:DataSource
+	write-host "running query"
+	$timestamp =  get-date -f _MM_dd_HH_mm_ss
+	$logFile = ".\msft_errors_" + $timestamp + "$ownerId.csv"
+	$report  | Export-Csv $logFile
 }
 
 CreateNewDirectoryEntry $startDirectory
 InitPreMigrationMaster $startDirectory
+Read-Host -Prompt "Enter to Generate Post Scan Report"
 GeneratePostScanReport $startDirectory
 
 #usage run as admin --> .\pre_migration_master.ps1 -startDirectory "c:\test"
