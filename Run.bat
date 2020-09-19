060aa0dab4be726fbffca6f9b15f5da86537f980
TNabbefeldHBS <66482010+TNabbefeldHBS@users.noreply.github.com>
2020-09-18 20:10:25 -0500

    Restructured files
    Removed "run.bat" and added documentation in "documentation.docx"

--- Run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -Argument '-NoProfile -ExecutionPolicy Bypass -File %~dp0pre_migration_master.ps1 -generateReports' -Verb RunAs}"
