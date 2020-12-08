9b4bf51bf6c774a54b0eb989ca05bea89235f088
TNabbefeldHBS <66482010+TNabbefeldHBS@users.noreply.github.com>
2020-12-08 12:07:05 -0600

    Updated database structure
    Updated the database structure to better prepare for migration.

--- create_db.ps1
+++ create_db.ps1
@@ -1,20 +1,17 @@
 $query = "  USE [FileToOneDrive]
-            
 
-            /****** Object:  Table [dbo].[Config]    Script Date: 7/21/2020 8:15:29 AM ******/
+            /****** Object:  Table [dbo].[Config]    Script Date: 8/11/2020 10:28:35 AM ******/
             SET ANSI_NULLS ON
-            
 
             SET QUOTED_IDENTIFIER ON
-            
 
             CREATE TABLE [dbo].[Config](
                 [Id] [int] IDENTITY(1,1) NOT NULL,
-                [source] [text] NULL,
-                [mode] [varchar](max) NULL,
-                [report] [varchar](max) NULL,
-                [notifications] [varchar](max) NULL,
-                [email] [varchar](max) NULL,
+                [mode] [nvarchar](max) NULL,
+                [source] [nvarchar](max) NULL,
+                [report] [nvarchar](max) NULL,
+                [notifications] [nvarchar](max) NULL,
+                [email] [nvarchar](max) NULL,
             PRIMARY KEY CLUSTERED 
             (
                 [Id] ASC
@@ -25,71 +22,78 @@ SqlQueryInsert($query)
 
 $query = "  USE [FileToOneDrive]
 
-            /****** Object:  Table [dbo].[Files_Batch_Users]    Script Date: 7/21/2020 8:16:08 AM ******/
+            /****** Object:  Table [dbo].[Files_Batch_Users]    Script Date: 8/4/2020 11:47:51 AM ******/
             SET ANSI_NULLS ON
 
             SET QUOTED_IDENTIFIER ON
 
             CREATE TABLE [dbo].[Files_Batch_Users](
                 [Id] [int] IDENTITY(1,1) NOT NULL,
+                [SamAccountName] [nvarchar](max) NULL,
+                [LastLogonDate] [datetime] NULL,
                 [BatchNumber] [int] NULL,
-                [ADHomeDirectory] [varchar](max) NULL,
-                [Counter] [int] NULL,
-                [OneDriveUrl] [int] NULL,
-                [SamAccountName] [varchar](max) NULL,
-            PRIMARY KEY CLUSTERED 
-            (
-                [Id] ASC
-            )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+                [BatchDate] [datetime] NULL,
+                [Building] [nvarchar](max) NULL,
+                [Title] [nvarchar](max) NULL,
+                [AllDayEvening] [nvarchar](max) NULL,
+                [Role] [nvarchar](max) NULL,
+                [ADHomeDirectory] [nvarchar](max) NULL,
+                [Counter] [float] NULL,
+                [ADMigrateGroupMember] [bit] NULL,
+                [UserPrincipalName] [nvarchar](max) NULL,
+                [OneDriveUrl] [nvarchar](max) NULL,
+                [TaskJson] [nvarchar](max) NULL
             ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]"
 
 SqlQueryInsert($query)
 
 $query = "  USE [FileToOneDrive]
 
-            /****** Object:  Table [dbo].[Files_OneDrive]    Script Date: 7/21/2020 8:16:42 AM ******/
+            /****** Object:  Table [dbo].[Files_OneDrive]    Script Date: 8/10/2020 10:25:53 AM ******/
             SET ANSI_NULLS ON
 
             SET QUOTED_IDENTIFIER ON
 
             CREATE TABLE [dbo].[Files_OneDrive](
                 [Id] [int] IDENTITY(1,1) NOT NULL,
-                [FileName] [text] NULL,
-                [Author] [text] NULL,
-                [Extension] [varchar](255) NULL,
-                [Folder01] [text] NULL,
-                [Folder02] [text] NULL,
-                [Folder03] [text] NULL,
-                [Folder04] [text] NULL,
-                [Folder05] [text] NULL,
-                [Folder06] [text] NULL,
-                [Folder07] [text] NULL,
-                [Folder08] [text] NULL,
-                [Folder09] [text] NULL,
-                [Folder10] [text] NULL,
-                [Folder11] [text] NULL,
-                [Folder12] [text] NULL,
-                [Folder13] [text] NULL,
-                [Folder14] [text] NULL,
-                [Folder15] [text] NULL,
-                [Folder16] [text] NULL,
-                [Folder17] [text] NULL,
-                [Folder18] [text] NULL,
-                [Folder19] [text] NULL,
-                [Folder20] [text] NULL,
+                [FileName] [nvarchar](max) NULL,
+                [Location] [nvarchar](max) NULL,
+                [Created] [float] NULL,
+                [Modified] [float] NULL,
+                [Author] [nvarchar](max) NULL,
+                [Extension] [nvarchar](max) NULL,
+                [Folder01] [nvarchar](max) NULL,
+                [Folder02] [nvarchar](max) NULL,
+                [Folder03] [nvarchar](max) NULL,
+                [Folder04] [nvarchar](max) NULL,
+                [Folder05] [nvarchar](max) NULL,
+                [Folder06] [nvarchar](max) NULL,
+                [Folder07] [nvarchar](max) NULL,
+                [Folder08] [nvarchar](max) NULL,
+                [Folder09] [nvarchar](max) NULL,
+                [Folder10] [nvarchar](max) NULL,
+                [Folder11] [nvarchar](max) NULL,
+                [Folder12] [nvarchar](max) NULL,
+                [Folder13] [nvarchar](max) NULL,
+                [Folder14] [nvarchar](max) NULL,
+                [Folder15] [nvarchar](max) NULL,
+                [Folder16] [nvarchar](max) NULL,
+                [Folder17] [nvarchar](max) NULL,
+                [Folder18] [nvarchar](max) NULL,
+                [Folder19] [nvarchar](max) NULL,
+                [Folder20] [nvarchar](max) NULL,
+                [Normalized] [bit] NULL,
                 [OwnerId] [int] NULL,
-                [Ignore] [varchar](255) NULL,
-                [Path] [text] NULL,
+                [Path] [nvarchar](max) NULL,
                 [FolderDepth] [int] NULL,
-                [ParentFolder] [text] NULL,
-                [RelativeFolder] [text] NULL,
-                [HasMacro] [int] NULL,
-                [OfficeOpen] [int] NULL,
+                [ParentFolder] [nvarchar](max) NULL,
+                [RelativeFolder] [nvarchar](max) NULL,
+                [HasMacro] [bit] NULL,
+                [OfficeOpen] [bit] NULL,
                 [PathLength] [int] NULL,
-                [Error] [varchar](max) NULL,
-                [Size] [numeric](38, 5) NULL,
-                [Created] [numeric](38, 5) NULL,
-                [Modified] [numeric](38, 5) NULL,
+                [Error] [nvarchar](max) NULL,
+                [Size] [float] NULL,
+                [Ignore] [bit] NULL
             PRIMARY KEY CLUSTERED 
             (
                 [Id] ASC
@@ -100,7 +104,7 @@ SqlQueryInsert($query)
 
 $query = "  USE [FileToOneDrive]
 
-            /****** Object:  Table [dbo].[Files_Users]    Script Date: 7/21/2020 8:17:41 AM ******/
+            /****** Object:  Table [dbo].[Files_Users]    Script Date: 8/11/2020 10:22:11 AM ******/
             SET ANSI_NULLS ON
 
             SET QUOTED_IDENTIFIER ON
@@ -110,26 +114,112 @@ $query = "  USE [FileToOneDrive]
                 [FileCountDisk] [int] NULL,
                 [FileCountCrawl] [int] NULL,
                 [MacroCount] [int] NULL,
-                [OfficeConversion] [int] NULL,
-                [OfficeCleanup] [int] NULL,
-                [Extensions] [text] NULL,
+                [OfficeConversion] [bit] NULL,
+                [OfficeCleanup] [bit] NULL,
+                [Extensions] [nvarchar](max) NULL,
+                [FileSizeDisk] [float] NULL,
+                [FileSizeCrawl] [float] NULL,
                 [ErrorCount] [int] NULL,
                 [OfficeErrorCount] [int] NULL,
                 [OldOfficeCount] [int] NULL,
                 [PathLengthCount] [int] NULL,
-                [Migration] [int] NULL,
+                [Migration] [bit] NULL,
                 [ApiNeedToMigrateCount] [int] NULL,
                 [ApiCompletedCount] [int] NULL,
                 [ApiErrorCount] [int] NULL,
-                [ApiReportPath] [int] NULL,
+                [ApiReportPath] [nvarchar](max) NULL,
                 [NoAccessCount] [int] NULL,
-                [CreatedDate] [int] NULL,
-                [FileSizeDisk] [numeric](38, 5) NULL,
-                [FileSizeCrawl] [numeric](38, 5) NULL,
+                [CreatedDate] [float] NULL
             PRIMARY KEY CLUSTERED 
             (
                 [OwnerId] ASC
             )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+            ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]"
+
+SqlQueryInsert($query)
+
+$query = "  USE [FileToOneDrive]
+
+            /****** Object:  Table [dbo].[Files_OneDrive_Conversion]    Script Date: 8/4/2020 11:51:33 AM ******/
+            SET ANSI_NULLS ON
+
+            SET QUOTED_IDENTIFIER ON
+
+            CREATE TABLE [dbo].[Files_OneDrive_Conversion](
+                [Id] [int] IDENTITY(1,1) NOT NULL,
+                [FileName] [nvarchar](max) NULL,
+                [Location] [nvarchar](max) NULL,
+                [Created] [datetime] NULL,
+                [Modified] [datetime] NULL,
+                [Author] [nvarchar](max) NULL,
+                [Extension] [nvarchar](max) NULL,
+                [Folder01] [nvarchar](max) NULL,
+                [Folder02] [nvarchar](max) NULL,
+                [Folder03] [nvarchar](max) NULL,
+                [Folder04] [nvarchar](max) NULL,
+                [Folder05] [nvarchar](max) NULL,
+                [Folder06] [nvarchar](max) NULL,
+                [Folder07] [nvarchar](max) NULL,
+                [Folder08] [nvarchar](max) NULL,
+                [Folder09] [nvarchar](max) NULL,
+                [Folder10] [nvarchar](max) NULL,
+                [Folder11] [nvarchar](max) NULL,
+                [Folder12] [nvarchar](max) NULL,
+                [Folder13] [nvarchar](max) NULL,
+                [Folder14] [nvarchar](max) NULL,
+                [Folder15] [nvarchar](max) NULL,
+                [Folder16] [nvarchar](max) NULL,
+                [Folder17] [nvarchar](max) NULL,
+                [Folder18] [nvarchar](max) NULL,
+                [Folder19] [nvarchar](max) NULL,
+                [Folder20] [nvarchar](max) NULL,
+                [Normalized] [bit] NULL,
+                [OwnerId] [int] NULL,
+                [Ignore] [bit] NULL,
+                [Path] [nvarchar](max) NULL,
+                [FolderDepth] [int] NULL,
+                [ParentFolder] [nvarchar](max) NULL,
+                [RelativeFolder] [nvarchar](max) NULL,
+                [HasMacro] [bit] NULL,
+                [OfficeOpen] [bit] NULL,
+                [PathLength] [int] NULL,
+                [Error] [nvarchar](max) NULL,
+                [Size] [float] NULL
+            ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]"
+
+SqlQueryInsert($query)
+
+$query = "  USE [FileToOneDrive]
+
+            /****** Object:  Table [dbo].[Files_Users_Conversion]    Script Date: 8/4/2020 11:52:09 AM ******/
+            SET ANSI_NULLS ON
+
+            SET QUOTED_IDENTIFIER ON
+
+            CREATE TABLE [dbo].[Files_Users_Conversion](
+                [OwnerId] [int] NULL,
+                [Email] [nvarchar](max) NULL,
+                [FileCountDisk] [int] NULL,
+                [FileCountCrawl] [int] NULL,
+                [MacroCount] [int] NULL,
+                [OfficeConversion] [bit] NULL,
+                [OfficeCleanup] [bit] NULL,
+                [Extensions] [nvarchar](max) NULL,
+                [FileSizeDisk] [float] NULL,
+                [FileSizeCrawl] [float] NULL,
+                [ErrorCount] [int] NULL,
+                [OfficeErrorCount] [int] NULL,
+                [OldOfficeCount] [int] NULL,
+                [PathLengthCount] [int] NULL,
+                [Migration] [bit] NULL,
+                [ApiNeedToMigrateCount] [int] NULL,
+                [ApiCompletedCount] [int] NULL,
+                [ApiErrorCount] [int] NULL,
+                [ApiReportPath] [nvarchar](max) NULL,
+                [NoAccessCount] [int] NULL,
+                [CreatedDate] [datetime] NULL,
+                [SpmtProcess] [int] NULL,
+                [SpmtMessage] [nvarchar](max) NULL
             ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]"
 
 SqlQueryInsert($query)
