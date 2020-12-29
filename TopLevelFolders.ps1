7f0d0de55f971f2fb7c45d7d738bcbf078b4758c
Joe Baldwin <joe@humboldttechgroup.com>
2020-12-29 15:15:57 -0600

    Adding empty batches template, top level folder batch generation

---
+++ TopLevelFolders.ps1
@@ -0,0 +1,18 @@
+﻿param (
+	[string]$path = 'c:\projects'
+)
+
+$tempFolders = @()
+
+$folders = Get-ChildItem -Directory -Path $path
+foreach($folder in $folders)
+{
+    $tempFolder = New-Object -TypeName PsObject
+    $tempFolder | Add-Member -MemberType NoteProperty -Name HomeDirectory  $folder.FullName
+    $tempFolder | Add-Member -MemberType NoteProperty -Name Email  ''
+    $tempFolders = $tempFolders + $tempFolder
+}
+
+$timestamp = get-date -f _MM_dd_HH_mm_ss
+$logFile = ".\Batches" + $timestamp + ".csv"
+$tempFolders | Export-Csv $logFile -NoTypeInformation
