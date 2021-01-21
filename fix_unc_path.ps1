5f1bba4640126514606cdfe4ae20cf8a98495b90
Joe Baldwin <joe@humboldttechgroup.com>
2021-01-21 13:51:16 -0600

    adding fix_unc_path

---
+++ fix_unc_path.ps1
@@ -0,0 +1,30 @@
+﻿$rows = Import-csv .\user_homedrives.csv
+foreach($row in $rows)
+{
+    if ($row.Directory -ne $null -AND $row.Directory.Trim() -ne '')
+    {
+        $homeDirectory = $row.Directory
+        if ($homeDirectory.StartsWith('\\'))
+        {
+            #do nothing
+        }
+        elseif($homeDirectory.StartsWith('\'))
+        {
+            $homeDirectory = '\' + $homeDirectory
+            $row.Directory = $homeDirectory
+        }
+
+    }
+}
+
+$newRows = @()
+foreach($row in $rows)
+{
+    if ($row.Directory -ne $null -AND $row.Directory.Trim() -ne '')
+    {
+        $newRows += $row
+    }
+}
+$rows = $newRows
+
+$rows | Export-CSV .\user_homedrives_fixed.csv -NoTypeInformation
